2af7d3c293a5c9a90518b6d5fef4da23e5617890
ashlybiju5207 <ashlybiju725@gmail.com>
2024-03-13 19:56:33 +0530

    largest of 3 nos

--- largest.py
+++ largest.py
@@ -3,11 +3,21 @@ b=input('Enter the  second no:')
 c=input('Enter the  third no:')
 
 
-if( b>a):
-    if(b>c):
-        print('The largest no is:',b)
-    else:
-        print('The largest no is:',c)
+if(a>b) and (a>c):
+    
+        print('The largest no is',a)
+     
 
-elif(b>c):
-        print('The largest no is:',c)
+elif(b>a) and (b>c):
+
+        print('The largest no is ',b)
+
+else:
+        print('The largest no is ',c)
+        
+
+
+
+    
+
+      
