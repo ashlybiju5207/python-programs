0a091620ea800a4b8b6206639920cb823d02ec62
ashlybiju5207 <ashlybiju725@gmail.com>
2024-03-17 18:43:40 +0530

    swap two nos

---
+++ swap.py
@@ -0,0 +1,11 @@
+a=input('Enter the first no:')
+b=input('Enter the second no:')
+print('The  values of a and b before swapping are:')
+print('value of a:',a)
+print('value of b:',b)
+c=a
+a=b
+b=c
+print('The swapped values of a and b are:')
+print('value of a:',a)
+print('value of b:',b)
